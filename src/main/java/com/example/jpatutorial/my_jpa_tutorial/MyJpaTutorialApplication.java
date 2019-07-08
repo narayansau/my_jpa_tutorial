c6dd6ab50d5e806f878db4cb955a2c0579a72fa3
Narayan Sau <narayansau@yahoo.com>
2019-07-08 08:44:19 +0530

    initial commit

---
+++ src/main/java/com/example/jpatutorial/my_jpa_tutorial/MyJpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpatutorial.my_jpa_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJpaTutorialApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run( MyJpaTutorialApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/example/jpatutorial/my_jpa_tutorial/MyJpaTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jpatutorial.my_jpa_tutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyJpaTutorialApplicationTests{
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
